9129d6b09b5680b2130ff6f847ccf125fea8ed97
bhavinpanchani <bhavinpanchani9@gmail.com>
2022-03-26 02:14:37 -0400

    add calculate distance from store logic

--- js/calculateDistance.js
+++ js/calculateDistance.js
@@ -1,27 +1,87 @@
 function distance(lon1, lat1, lon2, lat2) {
     var R = 6371; // Radius of the earth in km
-    var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
-    var dLon = (lon2-lon1).toRad();
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
-            Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var dLat = (lat2 - lat1).toRad();  // Javascript functions in radians
+    var dLon = (lon2 - lon1).toRad();
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c; // Distance in km
     return d;
-    }
+}
 
-    /** Converts numeric degrees to radians */
-    if (typeof(Number.prototype.toRad) === "undefined") {
-    Number.prototype.toRad = function() {
+/** Converts numeric degrees to radians */
+if (typeof (Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function () {
         return this * Math.PI / 180;
     }
-    }
+}
 
-    navigator.geolocation.getCurrentPosition(function(pos) {
+navigator.geolocation.getCurrentPosition(function (pos) {
     console.log(pos);
     console.log(
         distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385)
     );
     // document.write(distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385))
     document.getElementById("demo").innerHTML = distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385).toFixed(2) + " mi";
-});
+}, showError);
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("demo").innerHTML = ""
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("demo").innerHTML = ""
+            break;
+        case error.TIMEOUT:
+            document.getElementById("demo").innerHTML = ""
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("demo").innerHTML = ""
+            break;
+    }
+}
+
+
+
+
+
+
+
+// navigator.permissions && navigator.permissions.query({ name: 'geolocation' })
+//     .then(function (PermissionStatus) {
+//         if (PermissionStatus.state == 'granted') {
+//             function distance(lon1, lat1, lon2, lat2) {
+//                 var R = 6371; // Radius of the earth in km
+//                 var dLat = (lat2 - lat1).toRad();  // Javascript functions in radians
+//                 var dLon = (lon2 - lon1).toRad();
+//                 var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//                     Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+//                     Math.sin(dLon / 2) * Math.sin(dLon / 2);
+//                 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//                 var d = R * c; // Distance in km
+//                 return d;
+//             }
+
+//             /** Converts numeric degrees to radians */
+//             if (typeof (Number.prototype.toRad) === "undefined") {
+//                 Number.prototype.toRad = function () {
+//                     return this * Math.PI / 180;
+//                 }
+//             }
+
+//             navigator.geolocation.getCurrentPosition(function (pos) {
+//                 console.log(pos);
+//                 console.log(
+//                     distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385)
+//                 );
+//                 // document.write(distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385))
+//                 document.getElementById("demo").innerHTML = distance(pos.coords.longitude, pos.coords.latitude, -74.1315021, 40.4436385).toFixed(2) + " mi";
+//             });
+//         } else if (PermissionStatus.state == 'prompt') {
+//             document.getElementById("demo").innerHTML = "location not yet granted"
+//         } else {
+//             document.getElementById("demo").innerHTML = "location access denied"
+//         }
+//     })
--- js/isStoreOpen.js
+++ js/isStoreOpen.js
@@ -2,55 +2,55 @@ var today = new Date();
 var day = today.getDay();
 var hour = today.getHours();
 
-if(day>=0 && day<=4){
-    if(hour >= 5 && hour < 21){
-//    document.write(
-//    "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//    );
+if (day >= 0 && day <= 4) {
+    if (hour >= 5 && hour < 21) {
         document.write(
-        "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
+            "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
+        // );
     }
-    else if (hour >= 21 && hour <22){
-//        document.write(
-//        "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//        );
+    else if (hour >= 21 && hour < 22) {
         document.write(
-        "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
+            "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
+        // );
     }
-    else{
-//        document.write(
-//        "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//        );
+    else {
         document.write(
-        "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
+            "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
+        // );
     }
 }
-else if(day > 4){
-    if(hour >= 5 && hour < 23){
-//    document.write(
-//    "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//    );
+else if (day > 4) {
+    if (hour >= 5 && hour < 23) {
         document.write(
-        "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
+            "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
+        // );
     }
-    else if (hour >= 23 && hour <24){
-//        document.write(
-//        "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//        );
+    else if (hour >= 23 && hour < 24) {
         document.write(
-        "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
+            "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
+        // );
     }
-    else{
-//        document.write(
-//        "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-//        );
+    else {
         document.write(
-        "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
+            "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
         );
+        // document.write(
+        // "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
+        // );
     }
 }

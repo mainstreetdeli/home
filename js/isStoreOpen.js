a08fb915fa0c3bf1402f74eb30ce9582c7d849d3
bhavinpanchani <bhavinpanchani9@gmail.com>
2022-04-03 18:01:30 -0400

    clean up isStoreOpen.js file

--- js/isStoreOpen.js
+++ js/isStoreOpen.js
@@ -2,55 +2,38 @@ var today = new Date();
 var day = today.getDay();
 var hour = today.getHours();
 
+var sign;
+var signCssClass;
+
 if (day >= 0 && day <= 4) {
     if (hour >= 5 && hour < 21) {
-        document.write(
-            "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
-        // );
+        sign = "OPEN";
+        signCssClass = "open-sign";
     }
     else if (hour >= 21 && hour < 22) {
-        document.write(
-            "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
-        // );
+        sign = "CLOSING SOON";
+        signCssClass = "closing-soon-sign";
     }
     else {
-        document.write(
-            "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
-        // );
+        sign = "CLOSED";
+        signCssClass = "closed-sign";
     }
 }
 else if (day > 4) {
     if (hour >= 5 && hour < 23) {
-        document.write(
-            "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='open-sign' style='display: inline-block;'>OPEN</h3></div><br>"
-        // );
+        sign = "OPEN";
+        signCssClass = "open-sign";
     }
     else if (hour >= 23 && hour < 24) {
-        document.write(
-            "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='closing-soon-sign' style='display: inline-block;'>CLOSING SOON</h3></div><br>"
-        // );
+        sign = "CLOSING SOON";
+        signCssClass = "closing-soon-sign";
     }
     else {
-        document.write(
-            "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>"
-        );
-        // document.write(
-        // "<div><h3 class='closed-sign' style='display: inline-block;'>CLOSED</h3></div><br>"
-        // );
+        sign = "CLOSED";
+        signCssClass = "closed-sign";
     }
 }
+
+document.write(
+    `<div><h3 class='${signCssClass}' style='display: inline-block;'>${sign}</h3><h4 id='demo' style='display: inline-block; margin-left: 12px; color: grey;'><img id='' style='margin-bottom: 5px;' src='images/Eclipse-0.6s-31px.svg' alt='loading..'></h4></div><br>`
+);
